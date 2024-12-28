60e7add2f5d1000c9bc8e328ea951a6caefbf297
belli <bellinna.uong@outlook.com>
2024-12-28 18:48:01 +0100

    base GameObject and default value for player

---
+++ game/GameObject.java
@@ -0,0 +1,24 @@
+package game;
+
+public abstract class GameObject {
+    private String name;
+    private String description;
+
+    // Constructor
+    public GameObject(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    // Abstract method to define unique behavior for each object
+    public abstract void interact();
+}
--- game/Player.java
+++ game/Player.java
@@ -5,6 +5,14 @@ public class Player {
     int mana;
     int strength;
 
+    // Constructor with default values
+    public Player() {
+        this.health = 100;
+        this.mana = 10;
+        this.strength = 10;
+    }
+
+    // Constructor with custom values
     public Player(int health, int mana, int strength) {
         this.health = health;
         this.mana = mana;
@@ -13,6 +21,7 @@ public class Player {
 
     @Override
     public String toString() {
-        return "Player [health=" + health + ", mana=" + mana + ", strength=" + strength;
+        return "Player [health=" + health + ", mana=" + mana + ", strength=" + strength + "]";
     }
 }
+
