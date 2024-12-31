925c2b8268248c515e3cf92c4fc5013c6f668265
belli <bellinna.uong@outlook.com>
2024-12-31 19:18:24 +0100

    Cleaning combat system to be less redondant

--- game/objects/monster/Enemy.java
+++ game/objects/monster/Enemy.java
@@ -3,10 +3,12 @@ package game.objects.monster;
 import game.Player;
 import game.objects.GameObject;
 
+import java.util.Scanner;
+
 public class Enemy extends GameObject {
-    private final int damage;
-    protected int health;
-    private boolean fly;
+    private final int damage; // Fixed damage for the enemy
+    protected int health;     // Health points of the enemy
+    private boolean fly;      // Indicates if the enemy can fly
 
     /**
      * Constructor for Enemy
@@ -21,6 +23,7 @@ public class Enemy extends GameObject {
         this.fly = false;
     }
 
+    // Getter and Setter for flying capability
     public void setFly(boolean canFly) {
         this.fly = canFly;
     }
@@ -31,16 +34,76 @@ public class Enemy extends GameObject {
 
     @Override
     protected void performInteraction(Player player) {
-        System.out.println("The " + getName() + " attacks you and deals " + damage + " damage!");
-        player.setHealth(player.getHealth() - damage);
+        System.out.println("The " + getName() + " notices you and prepares to attack!");
+        combat(player);
+    }
 
-        if (player.getHealth() <= 0) {
-            System.out.println("You have been defeated by the " + getName() + "!");
-        } else {
-            System.out.println("Your remaining health: " + player.getHealth());
+    /**
+     * Method to handle combat logic
+     * @param player The player fighting the enemy
+     */
+    public void combat(Player player) {
+        System.out.println("You engage in combat with the " + getName() + "!");
+
+        while (this.health > 0 && player.getHealth() > 0) {
+            // Player's turn
+            System.out.println("\nChoose your action:");
+            System.out.println("1. Sword Attack");
+            System.out.println("2. Fireball Attack");
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+
+            int damageToEnemy = takePlayerAction(player, choice);
+
+            // Apply damage to the enemy
+            this.health -= damageToEnemy;
+            if (this.health <= 0) {
+                System.out.println("You have defeated the " + getName() + "!");
+                break;
+            }
+
+            // Enemy's turn
+            enemyAction(player);
+
+            // Check if the player is defeated
+            if (player.getHealth() <= 0) {
+                System.out.println("You have been defeated by the " + getName() + "!");
+            } else {
+                System.out.println("Your remaining health: " + player.getHealth());
+            }
         }
     }
 
-    // Method to handle combat logic
-    public void combat(Player player) {}
+    /**
+     * Handles the player's action during combat.
+     * @param player The player object
+     * @param choice The player's action choice
+     * @return The damage dealt to the enemy
+     */
+    protected int takePlayerAction(Player player, int choice) {
+        int damageToEnemy = 0;
+        if (choice == 1) {
+            if (this.canFly()) {
+                System.out.println("The " + getName() + " dodges your sword attack! It's flying too high.");
+            } else {
+                damageToEnemy = player.swordAttack();
+                System.out.println("You slash the " + getName() + " with your sword, dealing " + damageToEnemy + " damage.");
+            }
+        } else if (choice == 2) {
+            damageToEnemy = player.fireballAttack();
+            System.out.println("You hit the " + getName() + " with a fireball, dealing " + damageToEnemy + " damage.");
+        } else {
+            System.out.println("Invalid choice! You lose your turn.");
+        }
+        return damageToEnemy;
+    }
+
+    /**
+     * Handles the enemy's action during combat.
+     * @param player The player object
+     */
+    protected void enemyAction(Player player) {
+        System.out.println("The " + getName() + " attacks you, dealing " + this.damage + " damage!");
+        player.setHealth(player.getHealth() - this.damage);
+    }
 }
--- game/objects/monster/Harpy.java
+++ game/objects/monster/Harpy.java
@@ -3,7 +3,6 @@ package game.objects.monster;
 import game.Player;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class Harpy extends Enemy {
     private static final int MIN_DAMAGE = 10;
@@ -13,51 +12,15 @@ public class Harpy extends Enemy {
      * Constructor for Harpy
      */
     public Harpy() {
-        super("Harpy", 0, 50);
-        this.setFly(true);
+        super("Harpy", 0, 50); // Damage is handled dynamically for Harpy
+        this.setFly(true); // Harpy can fly
     }
 
     @Override
-    public void combat(Player player) {
-        System.out.println("You encounter a " + getName() + " that soars through the air!");
-
-        Scanner scanner = new Scanner(System.in);
+    protected void enemyAction(Player player) {
         Random random = new Random();
-
-        while (this.health > 0 && player.getHealth() > 0) {
-            // Player's turn
-            System.out.println("\nChoose your action:");
-            System.out.println("1. Sword Attack");
-            System.out.println("2. Fireball Attack");
-            int choice = scanner.nextInt();
-
-            int damageToEnemy = 0;
-            if (choice == 1) {
-                System.out.println("The " + getName() + " dodges your sword attack! It's flying too high.");
-            } else if (choice == 2) {
-                damageToEnemy = player.fireballAttack();
-                this.health -= damageToEnemy;
-                System.out.println("You hit the " + getName() + " with a fireball, dealing " + damageToEnemy + " damage. Remaining health: " + this.health);
-            } else {
-                System.out.println("Invalid choice! You lose your turn.");
-            }
-
-            // Check if the enemy is defeated
-            if (this.health <= 0) {
-                System.out.println("You defeated the " + getName() + "!");
-                break;
-            }
-
-            // Enemy's turn
-            int damage = random.nextInt(MAX_DAMAGE - MIN_DAMAGE + 1) + MIN_DAMAGE;
-            System.out.println("The " + getName() + " swoops down from the sky and attacks you, dealing " + damage + " damage!");
-            player.setHealth(player.getHealth() - damage);
-
-            if (player.getHealth() <= 0) {
-                System.out.println("You have been defeated by the " + getName() + "!");
-            } else {
-                System.out.println("Your remaining health: " + player.getHealth());
-            }
-        }
+        int damage = random.nextInt(MAX_DAMAGE - MIN_DAMAGE + 1) + MIN_DAMAGE;
+        System.out.println("The " + getName() + " swoops down and attacks you, dealing " + damage + " damage!");
+        player.setHealth(player.getHealth() - damage);
     }
 }
