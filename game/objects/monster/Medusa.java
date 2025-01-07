c0ba9b4a4eb0a3257faee7f7596dffbb1536261f
belli <bellinna.uong@outlook.com>
2025-01-07 23:53:21 +0100

    Condition de fin

--- game/objects/monster/Medusa.java
+++ game/objects/monster/Medusa.java
@@ -18,44 +18,44 @@ public class Medusa extends Enemy {
     }
 
     @Override
-    public void combat(Player player) {
-        System.out.println("You encounter a " + getName() + "!");
-
-        Scanner scanner = new Scanner(System.in);
+    protected void enemyAction(Player player) {
         Random random = new Random();
+        int damage = random.nextInt(MAX_DAMAGE - MIN_DAMAGE + 1) + MIN_DAMAGE;
+        System.out.println("The " + getName() + " attacks you, dealing " + damage + " damage!");
+        player.setHealth(player.getHealth() - damage);
+    }
+
+    @Override
+    public void combat(Player player) {
+        System.out.println("You engage in combat with the " + getName() + "!");
 
         while (this.health > 0 && player.getHealth() > 0) {
             // Player's turn
             System.out.println("\nChoose your action:");
             System.out.println("1. Sword Attack");
             System.out.println("2. Fireball Attack");
+            Scanner scanner = new Scanner(System.in);
             int choice = scanner.nextInt();
 
-            int damageToEnemy = 0;
-            if (choice == 1) {
-                damageToEnemy = player.swordAttack();
-            } else if (choice == 2) {
-                damageToEnemy = player.fireballAttack();
-            } else {
-                System.out.println("Invalid choice! You lose your turn.");
-            }
+            int damageToEnemy = takePlayerAction(player, choice);
 
+            // Apply damage to the enemy
             this.health -= damageToEnemy;
-            System.out.println("You deal " + damageToEnemy + " damage to the " + getName() + ". Remaining health: " + this.health);
-
-            // Check if the enemy is defeated
             if (this.health <= 0) {
-                System.out.println("You defeated the " + getName() + "!");
-                break;
+                this.defeated = true;  // Mark Medusa as defeated
+                System.out.println("You have defeated the " + getName() + "!");
+                System.out.println("Congratulations! You have saved the kingdom and completed the game!");
+                System.exit(0); // End the game
             }
 
             // Enemy's turn
-            int damage = random.nextInt(MAX_DAMAGE - MIN_DAMAGE + 1) + MIN_DAMAGE;
-            System.out.println("The " + getName() + " attacks you, dealing " + damage + " damage!");
-            player.setHealth(player.getHealth() - damage);
+            enemyAction(player);
 
+            // Check if the player is defeated
             if (player.getHealth() <= 0) {
                 System.out.println("You have been defeated by the " + getName() + "!");
+                System.out.println("Game Over.");
+                System.exit(0); // End the game
             } else {
                 System.out.println("Your remaining health: " + player.getHealth());
             }
